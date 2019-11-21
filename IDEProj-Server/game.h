8df72e0bef9c4f3550797d574263cf77d501466a
Rekongstor <rekongstor@mail.ru>
2019-11-21 23:57:45 +0300

    [Server] Создать класс game

--- IDEProj-Server/game.h
+++ IDEProj-Server/game.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "participant.h"
+
+class game
+{
+	enum game_states
+	{
+		preparation,
+		turn_1,
+		turn_2,
+		end
+	};
+public:
+	game(participant&, participant&);
+	bool place(int, int);
+	void set_state(game_states);
+	bool turn(int, int);
+
+private:
+	participant& m_participant_1;
+	participant& m_participant_2;
+	int m_field_1[10][10];
+	int m_field_2[10][10];
+};
