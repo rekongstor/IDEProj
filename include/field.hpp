34ffe7a415c86c3063757c89ff269e802481c931
Rekongstor <rekongstor@mail.ru>
2019-12-06 01:55:16 +0300

    [Field] Пофикшена проверка установки корабля

--- include/field.hpp
+++ include/field.hpp
@@ -35,22 +35,46 @@ public:
 
 	bool set_ship(int x, int y, int length, char direction)
 	{
+
+		auto check_bound = [](int x, int y)
+		{
+			return (x >= 0 && x <= 9 && y >= 0 && y <= 9);
+		};
+
 		if ((length < 1 || length > 4) ||
-			(direction == 'v' && y + length > 9) ||
-			(direction == 'h' && x + length > 9))
+			(direction == 'v' && y + length - 1 > 9) ||
+			(direction == 'h' && x + length - 1 > 9))
 		{
 			return false;
-		}
+		} // length check
 
-		for (int i = 0; i < length; ++i) {
-			if (m_field[x + i * (direction == 'h')][y + i * (direction == 'v')] == 1 ||
-				m_field[x + i * (direction == 'h') + 1][y + i * (direction == 'v')] == 1 ||
-				m_field[x + i * (direction == 'h') - 1][y + i * (direction == 'v')] == 1 ||
-				m_field[x + i * (direction == 'h')][y + i * (direction == 'v') + 1] == 1 ||
-				m_field[x + i * (direction == 'h')][y + i * (direction == 'v') - 1] == 1) {
-				return false;
-			}
-		}
+		for (int i = 0; i < length; ++i)
+			if (!check_bound(x + i * (direction == 'h'), y + i * (direction == 'v')))
+				return false; // bound check
+
+		for (int i = -1; i < length + 1; ++i) 
+		{ //-1 and +1 for corners
+
+			if (check_bound(x + i * (direction == 'h'), y + i * (direction == 'v'))) // to be sure if we're in m_field boundaries
+				if (m_field[x + i * (direction == 'h')][y + i * (direction == 'v')])
+					return false;
+
+			if (check_bound(x + i * (direction == 'h') + 1, y + i * (direction == 'v')))
+				if (m_field[x + i * (direction == 'h') + 1][y + i * (direction == 'v')])
+					return false;
+
+			if (check_bound(x + i * (direction == 'h') - 1, y + i * (direction == 'v')))
+				if (m_field[x + i * (direction == 'h') - 1][y + i * (direction == 'v')])
+					return false;
+
+			if (check_bound(x + i * (direction == 'h'), y + i * (direction == 'v') + 1))
+				if (m_field[x + i * (direction == 'h')][y + i * (direction == 'v') + 1])
+					return false;
+
+			if (check_bound(x + i * (direction == 'h'), y + i * (direction == 'v') - 1))
+				if (m_field[x + i * (direction == 'h')][y + i * (direction == 'v') - 1])
+					return false;
+		} // cross check
 
 		for (int i = 0; i < length; ++i)
 		{
